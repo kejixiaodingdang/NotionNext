e68b7129d21501784dbcc92302d6cefe691f58bc
kejixiaodingdang <kejixiaodingdang@protonmail.com>
2026-02-09 14:48:32 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '科技小叮当', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '💻 码农｜知识分享博主\n🚀 专注前端开发 · 向全栈工程师进阶\n📚 定期分享 AI / 科技互联网 / 软件工具 / 教程', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
